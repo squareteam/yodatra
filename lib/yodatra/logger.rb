85605d5fdc32714f7b77ffda3857c9fcd7ec6e29
Paul B <paul@bonaud.fr>
2014-02-02 15:12:43 +0100

    Bump version 0.1.3

--- lib/yodatra/logger.rb
+++ lib/yodatra/logger.rb
@@ -1,36 +1,20 @@
-require 'sinatra/logger'
-
 module Yodatra
   class Logger < Sinatra::Base
 
-    def call(env)
-      env['yodatra.logger'] ? @app.call(env) : super
+    configure do
+      enable :logging
+      filename = File.join(Dir.pwd, 'log', "#{environment}.log")
+      file = File.new(filename, 'a+')
+      file.sync = true
+      use Rack::CommonLogger, file
     end
 
-    superclass.class_eval do
-      alias call_without_check call unless method_defined? :call_without_check
-      def call(env)
-        env['sinatra.commonlogger'] = true
-        call_without_check(env)
-      end
-    end
-
-    filename = File.join(Dir.pwd, 'log', "#{environment}.log")
-
-    unless @env['yodatra.logger']
-      configure do
-        enable :logging
-        file = File.new(filename, 'a+')
-        file.sync = true
-        use Rack::CommonLogger, file
-        env['yodatra.logger'] = true
-      end
-    end
-
-    register Sinatra::Logger
-    app.set :logger_level, :debug unless app.environment == 'production'
+    #require 'sinatra/logger'
+    #register Sinatra::Logger
+    # set :logger_level, :debug unless environment == 'production'
     # set the full path to the log file
-    app.set :logger_log_file, lambda { filename }
+    # set :logger_log_file, lambda { filename }
 
   end
+
 end
--- lib/yodatra/version.rb
+++ lib/yodatra/version.rb
@@ -1,3 +1,3 @@
 module Yodatra
-  VERSION = '0.1.2-dev'
+  VERSION = '0.1.3'
 end
